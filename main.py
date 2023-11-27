74b92685cbbcfb03da2d23f5d5c730f8850ff77a
brucemug <mugzbru199@gmail.com>
2023-11-27 16:08:23 +0300

    comments for pipeline

---
+++ main.py
@@ -0,0 +1,29 @@
+# import packages
+
+
+# load models
+
+
+# read frames
+
+
+# detect vehicles
+
+
+# track vehicles
+
+
+# detect license plates
+
+
+# assign plate to car
+
+
+# crop and process the plates
+
+
+# read plates
+
+
+# write results
+
