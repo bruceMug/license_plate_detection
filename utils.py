570fb3f585c9aaffddb726620d126fd07aa2e5e1
brucemug <mugzbru199@gmail.com>
2023-11-27 16:09:49 +0300

    check plate format

--- utils.py
+++ utils.py
@@ -10,3 +10,10 @@ def write_csv(results, output_path):
     """ write content to csv file"""
     
     
+def license_complies_format(text):
+    """ check if license complies with the format
+        this is where you check if the plate is 7 characters long and also
+        the format of Ugandan plates i.e both old (i.e 4 alphabets and 3 numbers)
+        and new plates ()"""
+    
+    
