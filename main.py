aa0eafd273a96235dc0c4c217ba0f8bed808c5da
brucemug <mugzbru199@gmail.com>
2023-11-28 08:28:53 +0300

    reading frames

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 # import packages
-
+from ultralytics import YOLO
+import cv2
 
 # load models
+coco_model = YOLO('yolov8n.pt')   # detecting cars
+license_plate_detector = YOLO('./models/license_plate_detector.pt')  # detecting license plates
 
 
 # read frames
+cap = cv2.VideoCapture('./test_one.mp4')
+
+# to track the frame count
+frame_nmr = 0
+
+while True:
+    frame_nmr += 1
+    ret, frame = cap.read()
+    
+    # 
+    if ret and frame_nmr < 10:
+    
+        # detect vehicles
+        detections = coco_model(frame)  # returns a list of detections
+        print(detections)
+
+        # track vehicles
 
 
-# detect vehicles
+        # detect license plates
 
 
-# track vehicles
+        # assign plate to car
 
 
-# detect license plates
+        # crop and process the plates
 
 
-# assign plate to car
+        # read plates
 
 
-# crop and process the plates
-
-
-# read plates
-
-
-# write results
+        # write results
 
