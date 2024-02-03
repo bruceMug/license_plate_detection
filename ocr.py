b3a474731ccecc92f47fcd89786230d103d0a16c
brucemug <mugzbru199@gmail.com>
2024-02-03 10:24:44 +0300

    added recognition

--- ocr.py
+++ ocr.py
@@ -1,12 +1,13 @@
 import easyocr
-# import cv2
+import cv2
 
 print('Reached here')
-reader = easyocr.Reader(['en'], gpu=False) # this needs to run only once to load the model into memory
+reader = easyocr.Reader(['en'], gpu=False, detection='DB', recognition = 'Transformer') # this needs to run only once to load the model into memory
 print('Reached here')
 
-# image = cv2.imread('./test_files/test_two.jpg')
-# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image = cv2.imread('./test_files/test_two.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-result = reader.readtext('./test_files/test_two.jpg', detail = 0)
+# result = reader.readtext('./test_files/test_two.jpg', detail = 0)
+result = reader.readtext(image)
 print(f"The output is: {result}")
