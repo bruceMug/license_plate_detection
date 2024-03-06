9430ed159a94118006b4d9f05c69ebcd980b7b7b
brucemug <mugzbru199@gmail.com>
2024-03-06 19:15:54 +0300

    refactor

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ from plates import read_detect_plates
 from tqdm import tqdm
 
 mot_tracker = Sort()  # create instance of the SORT tracker
-
-# load models
-coco_model = YOLO('./models/yolov8n.pt')   # detecting cars
+detectModel = YOLO('./models/yolov8n.pt')   # detecting cars
 
 
 def main(video_path, output_path):
@@ -20,49 +18,36 @@ def main(video_path, output_path):
 
     # to track the frame count
     frame_nmr = -1
-    frame_array = []
-
-    frame_nmr += 1
-
-    while (cap.isOpened()):
-        ret, frame = cap.read()
-
-        if ret:
-            # frame = cv2.resize(frame, (640, 480))
-            results[frame_nmr] = {}
-
-            # ------------------------------------------------------------------------------vehicles
-            # detect vehicles
-            detections = coco_model(frame)[0]  # returns a list of detections
-            detections_ = []
-
-            for detection in tqdm(detections.boxes.data.tolist()):
-                x1, y1, x2, y2, confidence_score, class_id = detection
-
-                # draw bounding box
-                frame = cv2.rectangle(frame, (int(x1), int(
-                    y1)), (int(x2), int(y2)), (0, 255, 0), 1)
-
-                frame_array.append(frame)
-
-                if int(class_id) in vehicles:
-                    detections_.append([x1, y1, x2, y2, confidence_score])
-
-            # track vehicles
-            track_ids = mot_tracker.update(np.asarray(detections_))
-
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
-        else:
-            break
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(
         f"{output_path}/output_video.mp4", fourcc, 30, (1920, 1080))
-    for i in tqdm(range(len(frame_array))):
-        out.write(frame_array[i])
-    out.release()
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        frame = cv2.resize(frame, (1920, 1080))
+
+        # Detect vehicles
+        detections = detectModel(frame)[0]
+        detections_ = []
+
+        for detection in tqdm(detections.boxes.data.tolist()):
+            x1, y1, x2, y2, confidence_score, class_id = detection
+
+            frame = cv2.rectangle(frame, (int(x1), int(
+                y1)), (int(x2), int(y2)), (0, 255, 0), 1)
+
+            if int(class_id) in vehicles:
+                detections_.append([x1, y1, x2, y2, confidence_score])
+
+        track_ids = mot_tracker.update(np.asarray(detections_))
+        frame = read_detect_plates(frame, track_ids)
+        out.write(frame)
 
     cap.release()
+    out.release()
     cv2.destroyAllWindows()
-    exit
