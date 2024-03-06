abd34034afea3da9bcf5bc5ab31341b17bc1f3cc
brucemug <mugzbru199@gmail.com>
2024-03-06 15:18:53 +0300

    refactored app script

--- app.py
+++ app.py
@@ -1,70 +1,68 @@
-# import packages
 from ultralytics import YOLO
 import cv2
 from sort.sort import *
 from utils import get_car, read_license_plate, write_csv
 from plates import read_detect_plates
-
+from tqdm import tqdm
 
 mot_tracker = Sort()  # create instance of the SORT tracker
 
 # load models
 coco_model = YOLO('./models/yolov8n.pt')   # detecting cars
-# license_plate_detector = YOLO('./models/license_plate_detector.pt') # detecting license plates
 
-print('Reading video ...')
-# read frames
-cap = cv2.VideoCapture('./test_two.mp4')
 
-# vehicles of interest
-vehicles = [2, 3, 6, 5, 7]
-results = {}
+def main(video_path, output_path):
+    print('Reading video ...')
+    cap = cv2.VideoCapture(video_path)
 
-# to track the frame count
-frame_nmr = -1
+    vehicles = [2, 3, 6, 5, 7]  # vehicles of interest
+    results = {}
 
-while True:
+    # to track the frame count
+    frame_nmr = -1
+    frame_array = []
+
     frame_nmr += 1
-    ret, frame = cap.read()
 
-    #
-    if ret:
-        frame = cv2.resize(frame, (640, 480))
-        # print(frame_nmr)
-        results[frame_nmr] = {}
-        # print(results)
+    while (cap.isOpened()):
+        ret, frame = cap.read()
 
-        # ------------------------------------------------------------------------------vehicles
-        # detect vehicles
-        detections = coco_model(frame)[0]  # returns a list of detections
-        detections_ = []  # list of all bounding boxes
+        if ret:
+            # frame = cv2.resize(frame, (640, 480))
+            results[frame_nmr] = {}
 
-        for detection in detections.boxes.data.tolist():
-            x1, y1, x2, y2, confidence_score, class_id = detection
-            
-            # draw bounding box
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 1)
-            cv2.imshow('frame', frame)
+            # ------------------------------------------------------------------------------vehicles
+            # detect vehicles
+            detections = coco_model(frame)[0]  # returns a list of detections
+            detections_ = []
 
-            if int(class_id) in vehicles:
-                detections_.append([x1, y1, x2, y2, confidence_score])
+            for detection in tqdm(detections.boxes.data.tolist()):
+                x1, y1, x2, y2, confidence_score, class_id = detection
 
-        # track vehicles
-        track_ids = mot_tracker.update(np.asarray(detections_))
+                # draw bounding box
+                frame = cv2.rectangle(frame, (int(x1), int(
+                    y1)), (int(x2), int(y2)), (0, 255, 0), 1)
 
-        # output = read_detect_plates(frame, results, frame_nmr, track_ids)
-        # print('Forwarded the frames to ...')
-        # read_detect_plates(frame, results, frame_nmr, track_ids)
-        # print('=> Returned the plates')
-        # print('Started next frame')
-        # write_csv(output, './results.csv')
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+                frame_array.append(frame)
+
+                if int(class_id) in vehicles:
+                    detections_.append([x1, y1, x2, y2, confidence_score])
+
+            # track vehicles
+            track_ids = mot_tracker.update(np.asarray(detections_))
+
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #     break
+        else:
             break
-    else:
-        pass
 
-# print(output)
-# save results to file
-# write_csv(output, './results.csv')
-cap.release()
-cv2.destroyAllWindows()
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(
+        f"{output_path}/output_video.mp4", fourcc, 30, (1920, 1080))
+    for i in tqdm(range(len(frame_array))):
+        out.write(frame_array[i])
+    out.release()
+
+    cap.release()
+    cv2.destroyAllWindows()
+    exit
